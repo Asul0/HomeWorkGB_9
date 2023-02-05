e49b95fdb9c47d72d990cfb0156e5670c134d474
Volodya <volodya.atoyan@list.ru>
2023-02-05 12:27:42 +0300

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Напишите программу, которая реализует рекурсивный 
+//метод нахождения суммы натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите первое число:");
+int num = Convert.ToInt32(ReadLine());
+
+WriteLine("Введите второе число:");
+int num2 = Convert.ToInt32(ReadLine());
+
+
+void GapNumberSum(int num, int num2, int sum)
+{
+   if (num > num2)
+   {
+      WriteLine($"Сумма натуральных элементов в промежутке от первого до второго числа = {sum}");
+      return;
+   }
+   sum = sum + (num2--);
+   GapNumberSum(num, num2, sum);
+}
+
+GapNumberSum(num, num2, 0);
